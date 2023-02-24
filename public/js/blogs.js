b422fbc9b096195a2f2c15e834e4d212c30a6a9a
verzeilberg <verzeilberg@srs.nl>
2023-02-24 16:52:50 +0100

    WIP

--- public/js/blogs.js
+++ public/js/blogs.js
@@ -1,6 +1,6 @@
+/** Check if Blog is on- or offline and activate right switch */
 function checkOfflineOnline() {
     let onlineValue = $('input[name=online]').val();
-
     if (onlineValue == 1) {
         $("span#blogOnline").trigger("click");
     } else {
@@ -8,33 +8,31 @@ function checkOfflineOnline() {
     }
 }
 
+/** Load all javascript/jQuery stuff on page ready */
 $(document).ready(function () {
+    /** Initialise offline/online check */
     checkOfflineOnline();
 
+    /** Set blog offline when clicking on button */
     $("span#blogOffline").on("click", function () {
         $("span#blogOnline").children('i').removeClass('text-success');
         $(this).children('i').addClass('text-danger');
         $('input[name=online]').val(0);
     });
 
+    /** Set blog online when clicking on button */
     $("span#blogOnline").on("click", function () {
         $("#blogOffline").children('i').removeClass('text-danger');
         $(this).children('i').addClass('text-success');
         $('input[name=online]').val(1);
     });
 
-    $('#categories').on('shown.bs.modal', function() {});
-
-    /**
-     * Init timeshift
-     */
+    /** Init timeshift */
     $("#timeOnline, #timeOffline").timeshift({
         hourClock: 24
     });
 
-    /**
-     * Init dateshift
-     */
+    /** Init dateshift */
     $(".dateOnline, .dateOffline").dateshift({
         preappelement: '<i class="far fa-calendar-alt"></i>',
         preapp: 'app',
@@ -42,16 +40,9 @@ $(document).ready(function () {
         previousButtonText: '<i class="far fa-caret-square-left"></i>',
         dateFormat: 'dd-mm-yyyy'
     });
-
-
-    let categoryModal = new bootstrap.Modal(document.getElementById('categories'))
-
-    $("button#buttonCategoryModal").on("click", function () {
-        categoryModal.toggle();
-    });
-
 });
 
+/** Init file upload layout */
 $(document).on('change', ':file', function () {
     var input = $(this),
         numFiles = input.get(0).files ? input.get(0).files.length : 1,
